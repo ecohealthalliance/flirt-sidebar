8c5399865cde7f57a0988e43bdd583bc11568831
treyyoder <treyyoder@gmail.com>
2016-01-14 16:35:27 -0500

    Initial push of flirt-sidebar.

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'dit:sidebar-v2',
+  version: '0.0.1',
+  summary: 'Bundled version of sidebar-v2 for meteor',
+  git: 'https://github.com/dit/sidebar-v2',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.addFiles('js/leaflet-sidebar.min.js', 'client');
+  api.addFiles('css/leaflet-sidebar.css', 'client');
+});
